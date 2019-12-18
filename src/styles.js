b6ea4b530859da4601d55e82635ec1c4efbb91ad
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-18 22:05:06 +0200

    feat: created styles.js, copied burgerStyles into it, and set it as an export

---
+++ src/styles.js
@@ -0,0 +1,45 @@
+export const burgerStyles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    right: '14px',
+    top: '14px',
+  },
+  bmBurgerBars: {
+    background: '#039be5',
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000',
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px',
+    backgroundColor: '#424242',
+  },
+  bmCross: {
+    background: '#bdc3c7',
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%',
+  },
+  bmMenu: {
+    background: '#373a47',
+    fontSize: '1.15em',
+  },
+  bmMorphShape: {
+    fill: '#373a47',
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    backgroundColor: '#039be5',
+    padding: '0.8em',
+  },
+  bmItem: {
+    display: 'inline-block',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+  },
+};
