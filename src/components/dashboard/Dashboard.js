31547511567433ebd3f60eb88e7af4aa7b063362
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 05:51:13 +0200

    feat: updated the App, Navbar, and Dashboard components to reflect the overall changes made to the app and its layout

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,48 +1,22 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import React from 'react';
 
-import PlaceholderMessage from './PlaceholderMessage';
-import Notifications from './Notifications';
+import GenerateNames from '../names/GenerateNames';
+import DisplayNames from '../names/DisplayNames';
 
-class Dashboard extends Component {
-  render() {
-    const { auth, notifications } = this.props;
-
-    if (!auth.uid) {
-      return <Redirect to="/signin" />;
-    } else {
-      return (
-        <div className="dashboard container">
-          <div className="row">
-            <div className="col s12 m6">
-              <PlaceholderMessage />
-            </div>
-
-            <div className="col s12 m5 offset-m1">
-              <Notifications notifications={notifications} />
-            </div>
-          </div>
+const Dashboard = props => {
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6">
+          <GenerateNames />
         </div>
-      );
-    }
-  }
-}
 
-const mapStateToProps = state => ({
-  auth: state.firebase.auth,
-  notifications: state.firestore.ordered.notifications,
-});
+        <div className="col s12 m5 offset-m0.5">
+          <DisplayNames />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    {
-      collection: 'notifications',
-      limit: 3,
-      orderBy: ['time', 'desc'],
-    },
-  ])
-)(Dashboard);
+export default Dashboard;
