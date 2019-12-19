6e60e9c578abff0521b44c50b4a1ed9a1e543cb9
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 23:36:30 +0200

    feat: assigned prop types in the Navbar, Dashboard, DisplayNames, and Generate components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,14 @@
+// Imports
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import GenerateNames from '../names/GenerateNames';
 import DisplayNames from '../names/DisplayNames';
 
+// Component
 const Dashboard = props => {
+  // console.log('props in Dashboard: ', props);
+
   return (
     <div className="dashboard container">
       <div>
@@ -19,21 +24,25 @@ const Dashboard = props => {
 
         <div className="center-text padding-bottom">
           <div>Like your new Star Wars name?</div>
-          Tweet it at me{' '}
-          <a
-            href="https://twitter.com/LuigiLegion"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="bold-text-style">@LuigiLegion</span>
-          </a>
+
+          <div>
+            Tweet it at me{' '}
+            <a
+              href="https://twitter.com/LuigiLegion"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="bold-text-style">@LuigiLegion</span>
+            </a>
+          </div>
+
           <div>#StarWars #TheRiseOfSkywalker</div>
         </div>
 
         <div className="center-text padding-bottom">
           Made with ❤ by{' '}
           <a
-            href="http://taluigi.netlify.com"
+            href="https://www.linkedin.com/in/talluigi"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -46,3 +55,8 @@ const Dashboard = props => {
 };
 
 export default Dashboard;
+
+// Prop Types
+Dashboard.propTypes = {
+  props: PropTypes.object,
+};
