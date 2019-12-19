919d5359a5f20b51a929b1495bdea7963119f83f
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 05:50:08 +0200

    feat: merged both types of links and burger links into one due to the abcense of auth

---
+++ src/components/layout/LinksBurger.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
+
+import { burgerStyles } from '../../styles';
+
+class LinksBurger extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      menuOpen: false,
+    };
+
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
+
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu
+          isOpen={this.state.menuOpen}
+          onStateChange={state => this.handleStateChange(state)}
+          right
+          width="50%"
+          styles={burgerStyles}
+        >
+          <div className="remove-outline">
+            <div>
+              <NavLink onClick={() => this.closeMenu()} to="/placeholder">
+                <span className="bold-text-style">Placeholder</span>
+              </NavLink>
+            </div>
+          </div>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default LinksBurger;
