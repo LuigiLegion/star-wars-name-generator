d34bbff869cee48cb35e889821158313fe159440
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 23:37:31 +0200

    feat: created the PageNotFound component as a redirect for 404 errors

---
+++ src/components/404/PageNotFound.js
@@ -0,0 +1,50 @@
+// Imports
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+// Component
+const PageNotFound = props => {
+  // console.log('props in PageNotFound: ', props);
+
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m12">
+          <div className="section">
+            <div className="card z-depth-0">
+              <div className="card-content grey-text text-darken-3">
+                <span className="card-title">
+                  <span className="bold-text-style">404 - Page Not Found</span>
+                </span>
+
+                <h5 className="grey-text text-darken-3">
+                  This route does not exist.
+                </h5>
+
+                <br />
+
+                <ul>
+                  <li>
+                    <NavLink to="/">
+                      <span className="bold-text-style">
+                        ← Back To Main Page
+                      </span>
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
+
+// Prop Types
+PageNotFound.propTypes = {
+  props: PropTypes.object,
+};
