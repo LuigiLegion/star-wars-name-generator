9d272f0820f7cc7b166bb2e27a9e78e2c0108526
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 07:12:48 +0200

    feat: created the getFirstName thunk creator and imported the getName method and the necessary data sets to invoke it in namesReducer

--- src/store/reducers/namesReducer.js
+++ src/store/reducers/namesReducer.js
@@ -1,9 +1,16 @@
+import { maleFirstNames } from '../../data/sets/sorted/male/male-first-names';
+import { femaleFirstNames } from '../../data/sets/sorted/female/female-first-names';
+import { allLastNames } from '../../data/sets/sorted/all/all-last-names';
+import { allFirstNames } from '../../data/sets/sorted/all/all-first-names';
+
+import { getName } from '../../functions/generate';
+
 // Initial State
 const initialState = {
   firstNames: [],
 };
 
-// Action
+// Actions
 const GOT_FIRST_NAME = 'GOT_FIRST_NAME';
 
 // Action Creators
@@ -12,11 +19,41 @@ export const gotFirstNameActionCreator = firstName => ({
   firstName,
 });
 
+// Thunk Creators
+export const getFirstNameThunkCreator = (firstName, gender) => {
+  return dispatch => {
+    try {
+      // console.log('firstName in getFirstNameThunkCreator: ', firstName);
+      // console.log('gender in getFirstNameThunkCreator: ', gender);
+
+      let generatedFirstName;
+
+      if (gender === 'Male') {
+        generatedFirstName = getName(firstName, maleFirstNames);
+      } else if (gender === 'Female') {
+        generatedFirstName = getName(firstName, femaleFirstNames);
+      } else {
+        generatedFirstName = getName(firstName, allFirstNames);
+      }
+
+      // console.log(
+      //   'generatedFirstName in getFirstNameThunkCreator: ',
+      //   generatedFirstName
+      // );
+
+      dispatch(gotFirstNameActionCreator(generatedFirstName));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
 // Reducer
 const namesReducer = (state = initialState, action) => {
   switch (action.type) {
     case GOT_FIRST_NAME:
-      console.log('action.firstName in GOT_FIRST_NAMES: ', action.firstName);
+      // console.log('action.firstName in GOT_FIRST_NAMES: ', action.firstName);
+
       return { ...state, firstNames: [...state.firstNames, action.firstName] };
 
     default:
