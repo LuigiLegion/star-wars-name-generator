60335eeda3d241560f4c7fa93166cffffc27bd04
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 11:36:34 +0200

    feat: added links to the github repo, data sets, wikia api, and my contact info in the Links and LinksBurger components

--- src/components/layout/Links.js
+++ src/components/layout/Links.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const Links = props => {
   return (
     <ul className="right">
       <li>
-        <NavLink to="/placeholder">
-          <span className="bold-text-style glow-text-style">Placeholder</span>
-        </NavLink>
+        <a
+          href="https://github.com/LuigiLegion/star-wars-name-generator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="bold-text-style glow-text-style">GitHub Repo</span>
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://github.com/LuigiLegion/star-wars-name-generator/tree/master/src/data/sets"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="bold-text-style glow-text-style">Data Sets</span>
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.wikia.com/api/v1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="bold-text-style glow-text-style">Wikia API</span>
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="http://taluigi.netlify.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="bold-text-style glow-text-style">Contact Info</span>
+        </a>
       </li>
     </ul>
   );
