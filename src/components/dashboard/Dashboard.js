e28dad3e677f847508d0479cd37b6f1da079c144
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-19 11:35:37 +0200

    feat: added a made with love message and twitter profile url in the Dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,13 +6,39 @@ import DisplayNames from '../names/DisplayNames';
 const Dashboard = props => {
   return (
     <div className="dashboard container">
-      <div className="row">
-        <div className="col s12 m6">
-          <GenerateNames />
+      <div>
+        <div className="row">
+          <div className="col s12 m6">
+            <GenerateNames />
+          </div>
+
+          <div className="col s12 m5 offset-m0.5">
+            <DisplayNames />
+          </div>
         </div>
 
-        <div className="col s12 m5 offset-m0.5">
-          <DisplayNames />
+        <div className="center-text padding-bottom">
+          <div>Like your new Star Wars name?</div>
+          Tweet it at me{' '}
+          <a
+            href="https://twitter.com/LuigiLegion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="bold-text-style">@LuigiLegion</span>
+          </a>
+          <div>#StarWars #TheRiseOfSkywalker</div>
+        </div>
+
+        <div className="center-text padding-bottom">
+          Made with ❤ by{' '}
+          <a
+            href="http://taluigi.netlify.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="bold-text-style">Tal Luigi</span>
+          </a>
         </div>
       </div>
     </div>
