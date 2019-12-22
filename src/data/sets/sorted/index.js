d1937a06791cb9f01a8bc19dc02999e9f425614e
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-22 23:53:01 +0200

    feat: created index.js as a file that imports all datasets and exports them as a object with identifying set names

---
+++ src/data/sets/sorted/index.js
@@ -0,0 +1,35 @@
+const { allFirstNames } = require('./all/all-first-names.json');
+const { allLastNames } = require('./all/all-last-names.json');
+const { maleFirstNames } = require('./male/male-first-names.json');
+const { maleLastNames } = require('./male/male-last-names.json');
+const { femaleFirstNames } = require('./female/female-first-names.json');
+const { femaleLastNames } = require('./female/female-last-names.json');
+
+const datasets = [
+  {
+    collectionName: 'allFirstNames',
+    dataset: allFirstNames,
+  },
+  {
+    collectionName: 'allLastNames',
+    dataset: allLastNames,
+  },
+  {
+    collectionName: 'maleFirstNames',
+    dataset: maleFirstNames,
+  },
+  {
+    collectionName: 'maleLastNames',
+    dataset: maleLastNames,
+  },
+  {
+    collectionName: 'femaleFirstNames',
+    dataset: femaleFirstNames,
+  },
+  {
+    collectionName: 'femaleLastNames',
+    dataset: femaleLastNames,
+  },
+];
+
+module.exports = datasets;
