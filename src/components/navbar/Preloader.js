904dad170eef294d64b894f810badd4684827003
Tal Luigi <luigi.t.agr@gmail.com>
2020-05-05 12:12:42 -0400

    feat: created the Preloader component to show users a loading bar on asynchronous operations and placed it in the navbar directory together with the Navbar component

---
+++ src/components/navbar/Preloader.js
@@ -0,0 +1,13 @@
+// Imports
+import React from 'react';
+
+// Component
+const Preloader = () => {
+  return (
+    <div className="progress preloader preloader-inner-color">
+      <div className="indeterminate preloader-outer-color" />
+    </div>
+  );
+};
+
+export default Preloader;
