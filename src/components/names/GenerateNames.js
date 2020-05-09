bc2396b3fc4bc5088e32353d46bb566a48688de6
Tal Luigi <luigi.t.agr@gmail.com>
2020-05-09 11:28:01 -0400

    feat: refactored the GenerateNames stateful class component as a functional component using the useState hook

--- src/components/names/GenerateNames.js
+++ src/components/names/GenerateNames.js
@@ -1,101 +1,84 @@
 /* eslint-disable react/button-has-type */
 
 // Imports
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { getNamesThunkCreator } from '../../store/reducers/namesReducer';
 
 // Component
-export class GenerateNames extends Component {
-  constructor() {
-    super();
+const GenerateNames = ({ validInitial, getNamesThunk }) => {
+  const [state, setState] = useState({
+    firstName: '',
+    lastName: '',
+    // countryName: '',
+    gender: 'male',
+  });
 
-    this.state = {
-      gender: 'male',
-      firstName: '',
-      lastName: '',
-      // countryName: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
+  const handleChange = event => {
     // console.log('event.target.id: ', event.target.id);
     // console.log('event.target.value: ', event.target.value);
 
-    this.setState({
+    setState({
+      ...state,
       [event.target.id]: event.target.value,
     });
-  }
+  };
 
-  handleSubmit(event) {
+  const handleSubmit = event => {
     // console.log('event.target.id: ', event.target.id);
     // console.log('event.target.value: ', event.target.value);
 
     event.preventDefault();
+    // console.log('firstName in GenerateNames handleSubmit: ', state.firstName);
+    // console.log('lastName in GenerateNames handleSubmit: ', state.lastName);
+    // console.log('countryName in GenerateNames handleSubmit: ', state.countryName);
+    // console.log('gender in GenerateNames handleSubmit: ', state.gender);
 
-    const { getNamesThunk } = this.props;
-    const { gender, firstName, lastName } = this.state;
+    getNamesThunk(state.firstName, state.lastName, state.gender);
+  };
 
-    // console.log('gender in GenerateNames handleSubmit: ', gender);
-    // console.log('firstName in GenerateNames handleSubmit: ', firstName);
-    // console.log('lastName in GenerateNames handleSubmit: ', lastName);
-    // console.log('countryName in GenerateNames handleSubmit: ', countryName);
+  const namesCheck = state.firstName && state.lastName;
 
-    getNamesThunk(gender, firstName, lastName);
-  }
+  // console.log('validInitial in GenerateNames: ', validInitial);
+  // console.log('firstName in GenerateNames: ', state.firstName);
+  // console.log('lastName in GenerateNames: ', state.lastName);
+  // console.log('gender in GenerateNames: ', state.gender);
+  // console.log('namesCheck in GenerateNames: ', namesCheck);
 
-  render() {
-    const { validInitial } = this.props;
-    const { firstName, lastName } = this.state;
-    const namesCheck = firstName && lastName;
-
-    // console.log('validInitial in GenerateNames: ', validInitial);
-    // console.log('firstName in GenerateNames: ', firstName);
-    // console.log('lastName in GenerateNames: ', lastName);
-    // console.log('namesCheck in GenerateNames: ', namesCheck);
-
-    return (
-      <div className="container">
-        <div className="section center">
-          <form onSubmit={this.handleSubmit} className="card white">
-            <span className="card-title">
-              <span className="gray-text-color bold-text-style">
-                Generate Names
-              </span>
+  return (
+    <div className="container">
+      <div className="section center">
+        <form onSubmit={handleSubmit} className="card white">
+          <span className="card-title">
+            <span className="gray-text-color bold-text-style">
+              Generate Names
             </span>
+          </span>
 
-            <div className="input-field">
-              <label htmlFor="firstName">
-                Any First Name<span className="red-text-color">*</span>
-              </label>
+          <div className="input-field">
+            <label htmlFor="firstName">
+              Any First Name<span className="red-text-color">*</span>
+            </label>
 
-              <input
-                type="text"
-                id="firstName"
-                required
-                onChange={this.handleChange}
-              />
-            </div>
+            <input
+              type="text"
+              id="firstName"
+              required
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className="input-field">
-              <label htmlFor="lastName">
-                Any Last Name<span className="red-text-color">*</span>
-              </label>
+          <div className="input-field">
+            <label htmlFor="lastName">
+              Any Last Name<span className="red-text-color">*</span>
+            </label>
 
-              <input
-                type="text"
-                id="lastName"
-                required
-                onChange={this.handleChange}
-              />
-            </div>
+            <input type="text" id="lastName" required onChange={handleChange} />
+          </div>
 
-            {/* <div className="input-field">
+          {/* <div className="input-field">
               <label htmlFor="countryName">
                 Country Name<span className="red-text-color">*</span>
               </label>
@@ -104,57 +87,56 @@ export class GenerateNames extends Component {
                 type="text"
                 id="countryName"
                 required
-                onChange={this.handleChange}
+                onChange={handleChange}
               />
             </div> */}
 
-            <div className="input-field col s12">
-              <label htmlFor="gender">
-                Gender<span className="red-text-color">*</span>
-              </label>
+          <div className="input-field col s12">
+            <label htmlFor="gender">
+              Gender<span className="red-text-color">*</span>
+            </label>
 
-              <br />
-              <br />
+            <br />
+            <br />
 
-              <select
-                id="gender"
-                className="browser-default"
-                required
-                onChange={this.handleChange}
-              >
-                <option value="" disabled>
-                  --Please choose an option--
-                </option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="all">Other</option>
-              </select>
-            </div>
-
-            <button
-              className="btn black lighten-1 z-depth-0"
-              disabled={!namesCheck}
+            <select
+              id="gender"
+              className="browser-default"
+              required
+              onChange={handleChange}
             >
-              Generate
-            </button>
+              <option value="" disabled>
+                --Please choose an option--
+              </option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="all">Other</option>
+            </select>
+          </div>
 
-            {validInitial ? null : (
-              <Fragment>
-                <div className="red-text-color bold-text-style center-text-align">
-                  Names in a galaxy far far away usually start with a letter.
-                </div>
+          <button
+            className="btn black lighten-1 z-depth-0"
+            disabled={!namesCheck}
+          >
+            Generate
+          </button>
 
-                <div className="red-text-color bold-text-style center-text-align">
-                  Please check your name inputs.
-                </div>
-              </Fragment>
-            )}
-          </form>
-        </div>
+          {validInitial ? null : (
+            <Fragment>
+              <div className="red-text-color bold-text-style center-text-align">
+                Names in a galaxy far far away usually start with a letter.
+              </div>
+
+              <div className="red-text-color bold-text-style center-text-align">
+                Please check your name inputs.
+              </div>
+            </Fragment>
+          )}
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 // Container
 const mapStateToProps = state => ({
@@ -162,8 +144,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  getNamesThunk(gender, firstName, lastName) {
-    dispatch(getNamesThunkCreator(gender, firstName, lastName));
+  getNamesThunk(firstName, lastName, gender) {
+    dispatch(getNamesThunkCreator(firstName, lastName, gender));
   },
 });
 
