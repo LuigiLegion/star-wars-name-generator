35bddb2e3418ac173ec145b05cd13181e7a34a31
Tal Luigi <luigi.t.agr@gmail.com>
2020-05-08 23:36:10 -0400

    feat: refactored the LinksBurger stateful class component as a functional component using the useState hook

--- src/components/Links/LinksBurger.js
+++ src/components/Links/LinksBurger.js
@@ -1,89 +1,76 @@
 // Imports
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 
 import { burgerStyles } from '../../styles';
 
 // Component
-class LinksBurger extends Component {
-  constructor() {
-    super();
+const LinksBurger = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    this.state = {
-      menuOpen: false,
-    };
+  const handleStateChange = state => {
+    setMenuOpen(state.isOpen);
+  };
 
-    this.handleStateChange = this.handleStateChange.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
-  }
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
-  handleStateChange(state) {
-    this.setState({ menuOpen: state.isOpen });
-  }
-
-  closeMenu() {
-    this.setState({ menuOpen: false });
-  }
-
-  render() {
-    return (
-      <div>
-        <Menu
-          isOpen={this.state.menuOpen}
-          onStateChange={state => this.handleStateChange(state)}
-          right
-          width="50%"
-          styles={burgerStyles}
-        >
-          <div className="remove-outline">
-            <div>
-              <NavLink to="/about" onClick={() => this.closeMenu()}>
-                <span className="bold-text-style glow-text-style">About</span>
-              </NavLink>
-            </div>
-
-            <div>
-              <a
-                onClick={() => this.closeMenu()}
-                href="https://github.com/LuigiLegion/star-wars-name-generator"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="bold-text-style glow-text-style">Source</span>
-              </a>
-            </div>
-
-            <div>
-              <a
-                onClick={() => this.closeMenu()}
-                href="https://github.com/LuigiLegion/star-wars-name-generator/tree/master/src/data/sets"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="bold-text-style glow-text-style">
-                  Data Sets
-                </span>
-              </a>
-            </div>
-
-            <div>
-              <a
-                onClick={() => this.closeMenu()}
-                href="https://taluigi.netlify.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="bold-text-style glow-text-style">
-                  Contact Info
-                </span>
-              </a>
-            </div>
+  return (
+    <div>
+      <Menu
+        isOpen={menuOpen}
+        onStateChange={state => handleStateChange(state)}
+        right
+        width="50%"
+        styles={burgerStyles}
+      >
+        <div className="remove-outline">
+          <div>
+            <NavLink to="/about" onClick={() => closeMenu()}>
+              <span className="bold-text-style glow-text-style">About</span>
+            </NavLink>
           </div>
-        </Menu>
-      </div>
-    );
-  }
-}
+
+          <div>
+            <a
+              onClick={() => closeMenu()}
+              href="https://github.com/LuigiLegion/star-wars-name-generator"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="bold-text-style glow-text-style">Source</span>
+            </a>
+          </div>
+
+          <div>
+            <a
+              onClick={() => closeMenu()}
+              href="https://github.com/LuigiLegion/star-wars-name-generator/tree/master/src/data/sets"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="bold-text-style glow-text-style">Data Sets</span>
+            </a>
+          </div>
+
+          <div>
+            <a
+              onClick={() => closeMenu()}
+              href="https://taluigi.netlify.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="bold-text-style glow-text-style">
+                Contact Info
+              </span>
+            </a>
+          </div>
+        </div>
+      </Menu>
+    </div>
+  );
+};
 
 export default LinksBurger;
