782e9aaefae6e2a0646c846877ad92711e13d0df
Tal Luigi <luigi.t.agr@gmail.com>
2020-05-18 20:45:43 -0400

    feat: refactored the toastNotificationGenerator explicitly returning helper function into the toastNotification implicitly returning helper function

--- src/helpers/generators/toastNotificationGenerator.js
+++ src/helpers/generators/toastNotificationGenerator.js
@@ -1,10 +1,9 @@
 // Initializations
-const toastNotificationGenerator = (message, color) => {
+const toastNotification = (message, color) =>
   window.M.toast({
     html: `<span>${message}</span>`,
     classes: color,
   });
-};
 
 // Exports
-export default toastNotificationGenerator;
+export default toastNotification;
