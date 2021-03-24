03bf1003c80446b4ec1289864fed99ec15ed6312
Tal Luigi <luigi.t.agr@gmail.com>
2021-03-24 07:06:40 -0400

    feat: created the components index file with component export statements to be used as a centralized import location for all components

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// Exports
+export { default as PageNotFound } from './404/PageNotFound';
+export { default as About } from './dashboard/About';
+export { default as Dashboard } from './dashboard/Dashboard';
+export { default as Links } from './links/Links';
+export { default as LinksBurger } from './links/LinksBurger';
+export { default as NamesDisplay } from './names/NamesDisplay';
+export { default as NamesGenerate } from './names/NamesGenerate';
+export { default as Navbar } from './navbar/Navbar';
+export { default as Preloader } from './navbar/Preloader';
