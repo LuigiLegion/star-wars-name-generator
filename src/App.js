e4e6cbf444c9395f728b2a2c8af28599b0d7dfde
Tal Luigi <luigi.t.agr@gmail.com>
2021-04-08 19:58:46 -0400

    feat: refactored the App component to use the Routes component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 // Imports
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { Navbar, About, Dashboard, PageNotFound } from './components';
+import { Navbar } from './components';
+import Routes from './Routes';
 
 // Component
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar>Star Wars Name Generator</Navbar>
+    <div className="App">
+      <Navbar />
 
-        <Switch>
-          <Route exact path="/about" component={About} />
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/:wildcard" component={PageNotFound} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+      <Routes />
+    </div>
   );
 };
 
