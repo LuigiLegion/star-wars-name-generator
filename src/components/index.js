de3584377c62819cb1736d2a8260a0d4fdf08da8
Tal Luigi <luigi.t.agr@gmail.com>
2021-05-07 23:35:52 -0400

    chore: added export statements for the Disclaimer, MadeBy, and SuggestionBox components in the components index file

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,9 @@
 export { default as PageNotFound } from './404/PageNotFound';
 export { default as About } from './about/About';
 export { default as Dashboard } from './dashboard/Dashboard';
+export { default as Disclaimer } from './dashboard/Disclaimer';
+export { default as MadeBy } from './dashboard/MadeBy';
+export { default as SuggestionBox } from './dashboard/SuggestionBox';
 export { default as Links } from './links/Links';
 export { default as LinksBurger } from './links/LinksBurger';
 export { default as NamesDisplay } from './names/NamesDisplay';
