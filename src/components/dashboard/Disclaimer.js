03556b145fde2b4800327cc483d374d11d9fc977
Tal Luigi <luigi.t.agr@gmail.com>
2021-05-07 21:42:29 -0400

    feat: created the Disclaimer component

---
+++ src/components/dashboard/Disclaimer.js
@@ -0,0 +1,32 @@
+// Imports
+import React from 'react';
+
+// Component
+const Disclaimer = () => {
+  return (
+    <div className="text-align-center padding-bottom">
+      <div>
+        <span className="text-style-bold">Disclaimer:</span> This is a fan site
+        intended to encourage interest in Star Wars. This site is not an
+        official publication and is not in any way, shape, or form affiliated
+        with or endorsed by Star Wars, Lucasfilm, Disney, or any of their
+        licensees.
+      </div>
+
+      <div>
+        Copyrights and trademarks for the films, TV series, and any other
+        materials are held by their respective owners and reference to them is
+        allowed under the fair use clause of copyright law.
+      </div>
+
+      <div>
+        This page is in no way intended to harm or undermine the market value of
+        Star Wars, Lucasfilm, or Disney, rather to encourage fans to engage with
+        and seek out knowledge of characters set in the Star Wars universe.
+      </div>
+    </div>
+  );
+};
+
+// Exports
+export default Disclaimer;
