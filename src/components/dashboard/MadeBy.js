3ef5797dfe158058990b8b320c34c052c9a64bd1
Tal Luigi <luigi.t.agr@gmail.com>
2021-05-07 23:34:43 -0400

    feat: created the MadeBy component

---
+++ src/components/dashboard/MadeBy.js
@@ -0,0 +1,21 @@
+// Imports
+import React from 'react';
+
+// Component
+const MadeBy = () => {
+  return (
+    <div className="text-align-center padding-bottom">
+      Made with ❤ by{' '}
+      <a
+        href="https://www.linkedin.com/in/talluigi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="text-style-bold">Tal Luigi</span>
+      </a>
+    </div>
+  );
+};
+
+// Exports
+export default MadeBy;
