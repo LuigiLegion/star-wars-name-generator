a3ff7aad94810a3a557bb4ed882aa34deb53383a
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-06 23:35:30 -0400

    chore: reordered import statements in the client index file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import store from './store';
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
