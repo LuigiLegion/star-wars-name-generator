6ca7444f43565cf1bade8ced394fce3a8793427e
Tal Luigi <luigi.t.agr@gmail.com>
2021-08-08 00:29:29 -0400

    feat: added a Route leading to the Changelog component in the Routes component

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { withRouter, Switch, Route } from 'react-router-dom';
 
-import { About, Dashboard, PageNotFound } from './components';
+import { About, Changelog, Dashboard, PageNotFound } from './components';
 
 // Component
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/about" component={About} />
+      <Route exact path="/changelog" component={Changelog} />
       <Route exact path="/" component={Dashboard} />
       <Route path="/:wildcard" component={PageNotFound} />
     </Switch>
