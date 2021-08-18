a1f98ba27f146a151b8afaec6495e21486af52f4
Tal Luigi <luigi.t.agr@gmail.com>
2021-08-17 23:45:03 -0400

    chore: moved the toastNotification utility function as toast, moved it into the materialize directory under the utils directory, and updated the utils index file accordingly

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 // Exports
 export { randomNameByRandomRating, nameScore, fullNameScore, randomInitial, randomElement } from './generators/names';
-export { default as toastNotification } from './generators/toastNotification';
+export { default as toast } from './materialize/toast';
