37c2b2454ea4e76bf03cb52d500eb80e3d1e554a
Tal Luigi <luigi.t.agr@gmail.com>
2021-08-30 00:01:02 -0400

    chore: updated the names utility functions export statement in the utils index file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 // Exports
-export { randomNameByRandomRating, nameScore, fullNameScore, randomInitial, randomElement } from './matchers/names';
+export { randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
 export { default as toast } from './materialize/toast';
