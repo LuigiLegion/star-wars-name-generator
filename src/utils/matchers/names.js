1efe399cff97549c2e3f8f32494a35128ff63b63
Tal Luigi <luigi.t.agr@gmail.com>
2021-08-29 23:59:48 -0400

    feat: created the matchingLetterIndeces function to capture the indeces of matching letters in the randomly selected generated name, and refactored the nameScore, nameObject, nameListsByRating and randomNameByRandomRating functions accordingly

--- src/utils/matchers/names.js
+++ src/utils/matchers/names.js
@@ -11,7 +11,7 @@ const arrayOfEmptyArrays = length => [...Array(length)].map(_ => []);
 
 const arrayOfNonEmptyArrays = arrayOfArrays => arrayOfArrays.filter(array => array.length);
 
-const nameScore = (inputName, nameObject) => (nameObject.rating / inputName.length) * 100;
+const nameScore = (inputNameLength, generatedNameRating) => (generatedNameRating / inputNameLength) * 100;
 
 const fullNameScore = (firstNameScore, lastNameScore) => (firstNameScore * 0.5) + (lastNameScore * 0.5);
 
@@ -41,10 +41,41 @@ const nameRating = (inputName, starWarsName) => {
   return starWarsNameRating;
 };
 
-const nameObject = (name, rating) => ({
-  name,
-  rating,
-});
+const matchingLetterIndeces = (inputName, starWarsName) => {
+  const indeces = [];
+  let inputNamePointer = 0;
+  let starWarsNamePointer = 0;
+  let letterFoundPointer = 0;
+
+  while (inputNamePointer < inputName.length) {
+    const inputNameLetter = inputName[inputNamePointer];
+    const starWarsNameLetter = starWarsName[starWarsNamePointer];
+
+    if (starWarsNameLetter === inputNameLetter) {
+      indeces.push(starWarsNamePointer);
+      inputNamePointer++;
+      starWarsNamePointer++;
+      letterFoundPointer = starWarsNamePointer;
+    } else if (starWarsNamePointer < starWarsName.length) {
+      starWarsNamePointer++;
+    } else {
+      inputNamePointer++;
+      starWarsNamePointer = letterFoundPointer;
+    }
+  }
+
+  return indeces;
+};
+
+const nameObject = (inputName, starWarsName) => {
+  const matches = matchingLetterIndeces(inputName.toLowerCase(), starWarsName.toLowerCase());
+
+  return {
+    name: starWarsName,
+    score: nameScore(inputName.length, matches.length),
+    matches,
+  }
+};
 
 const nameListsByRating = (inputName, starWarsNames) => {
   const starWarsNameListsByRating = arrayOfEmptyArrays(inputName.length + 1);
@@ -52,14 +83,14 @@ const nameListsByRating = (inputName, starWarsNames) => {
   for (let i = 0; i < starWarsNames.length; i++) {
     const starWarsName = starWarsNames[i];
     const starWarsNameRating = nameRating(inputName, starWarsName.toLowerCase());
-    starWarsNameListsByRating[starWarsNameRating].push(nameObject(starWarsName, starWarsNameRating));
+    starWarsNameListsByRating[starWarsNameRating].push(starWarsName);
   }
 
   return arrayOfNonEmptyArrays(starWarsNameListsByRating);
 };
 
 const randomNameByRandomRating = (inputName, starWarsNames) =>
-  randomElement(randomElement(nameListsByRating(inputName.toLowerCase(), starWarsNames)));
+  nameObject(inputName, randomElement(randomElement(nameListsByRating(inputName.toLowerCase(), starWarsNames))));
 
 // Exports
-export { randomNameByRandomRating, nameScore, fullNameScore, randomInitial, randomElement };
+export { randomNameByRandomRating, fullNameScore, randomInitial, randomElement };
