4c64c58798571f823694dcae4d363466d43bf107
Tal Luigi <luigi.t.agr@gmail.com>
2021-09-04 11:21:35 -0400

    chore: replaced initialIsValid with nameIsValid in the names utils module export statement in the utils index file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 // Exports
-export { initialIsValid, randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
+export { nameIsValid, randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
 export { default as toast } from './materialize/toast';
