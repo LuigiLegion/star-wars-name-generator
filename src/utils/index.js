a9a33c326efab4ce878d2f8b7dda6d9ae62ef231
Tal Luigi <luigi.t.agr@gmail.com>
2021-09-11 00:48:09 -0400

    chore: renamed nameIsValid as isValidName in the names utils module export statement in the utils index file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 // Exports
-export { nameIsValid, randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
+export { isValidName, randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
 export { default as toast } from './materialize/toast';
