f1c6b9ea0f83818265646fd6e4fc172a06a45d63
Tal Luigi <luigi.t.agr@gmail.com>
2022-01-03 17:11:31 -0500

    feat: refactored the handleSpeak method, renamed it as handleReadAloud, moved it to the Dashboard component, added the Favorites component as a child component to the Dashboard component, and passed handleReadAloud as a prop to the Names and Favorites components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,29 +2,51 @@
 import React from 'react';
 
 import {
-  NamesGenerate,
-  NamesDisplay,
+  Generate,
+  Names,
+  Favorites,
   SuggestionBox,
   Disclaimer,
   MadeBy,
 } from '..';
 
+// Initializations
+const synth = window.speechSynthesis;
+let voices;
+const populateVoices = () => {
+  voices = synth.getVoices();
+};
+synth.onvoiceschanged = populateVoices;
+
+populateVoices();
+
 // Component
 const Dashboard = () => {
+  const handleReadAloud = fullName => {
+    const utterance = new SpeechSynthesisUtterance(fullName);
+    utterance.voice = voices.find(voice => voice.voiceURI === 'Samantha');
+
+    synth.speak(utterance);
+  };
+
   return (
     <>
-      <div className="row padding-bottom">
-        <NamesGenerate />
+      <div className="row">
+        <Generate />
 
-        <NamesDisplay />
+        <Names handleReadAloud={handleReadAloud} />
+      </div>
+
+      <div className="row favorites-list-container">
+        <Favorites handleReadAloud={handleReadAloud} />
       </div>
 
       <div className="container">
-      <SuggestionBox />
+        <SuggestionBox />
 
-      <Disclaimer />
+        <Disclaimer />
 
-      <MadeBy />
+        <MadeBy />
       </div>
     </>
   );
