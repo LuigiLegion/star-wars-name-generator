325e9ed0920e5beb653060650b2f9f96cc5247a1
Tal Luigi <luigi.t.agr@gmail.com>
2022-01-05 00:41:40 -0500

    feat: created the downloadDataFile utility function in the downloadDataFile utils module

---
+++ src/utils/window/downloadDataFile.js
@@ -0,0 +1,30 @@
+// Imports
+import moment from 'moment';
+
+import { toast } from '..';
+
+// Initializations
+const downloadDataFile = (dataStr, type, fileNamePrefix, fileNameExtension) => {
+  try {
+    const blob = new Blob([dataStr], { type });
+    const blobUrl = window.URL.createObjectURL(blob);
+    const fileName = `${fileNamePrefix}_${moment().format('MM_DD_YYYY_hh_mm_ss')}.${fileNameExtension}`;
+
+    const a = document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href', blobUrl);
+    a.setAttribute('download', fileName);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    window.URL.revokeObjectURL(blobUrl);
+    toast('File downloaded', 'green');
+  } catch (error) {
+    console.error(error);
+    toast('Error! Failed to download file', 'red');
+  }
+};
+
+// Exports
+export default downloadDataFile;
