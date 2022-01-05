0e7c33da5f6a9888ff869dc2f65e767a171ca98c
Tal Luigi <luigi.t.agr@gmail.com>
2022-01-05 01:09:10 -0500

    chore: added formatFavorites, jsonToCsv, copyToClipboard, and downloadDataFile export statements in the utils index file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
 // Exports
+export { default as formatFavorites } from './formatters/favorites';
 export { isValidName, randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
 export { default as toast } from './materialize/toast';
+export { default as jsonToCsv } from './parsers/jsonToCsv';
+export { default as copyToClipboard } from './window/copyToClipboard';
+export { default as downloadDataFile } from './window/downloadDataFile';
