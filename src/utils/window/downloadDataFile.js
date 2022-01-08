e22ddb738f8b34138e289c6078a3ebc52378fd68
Tal Luigi <luigi.t.agr@gmail.com>
2022-01-08 02:17:08 -0500

    feat: refactored the a element creation and clicking logic in the downloadDataFile utility function into the downloadElement and clickDownloadElement utility functions

--- src/utils/window/downloadDataFile.js
+++ src/utils/window/downloadDataFile.js
@@ -4,19 +4,28 @@ import moment from 'moment';
 import { toast } from '..';
 
 // Initializations
+const downloadElement = (blobUrl, fileName) => {
+  const a = document.createElement('a');
+  a.setAttribute('href', blobUrl);
+  a.setAttribute('download', fileName);
+  a.setAttribute('hidden', '');
+
+  return a;
+};
+
+const clickDownloadElement = a => {
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
+
 const downloadDataFile = (dataStr, type, fileNamePrefix, fileNameExtension) => {
   try {
     const blob = new Blob([dataStr], { type });
     const blobUrl = window.URL.createObjectURL(blob);
     const fileName = `${fileNamePrefix}_${moment().format('MM_DD_YYYY_HH_mm_ss')}.${fileNameExtension}`;
 
-    const a = document.createElement('a');
-    a.setAttribute('hidden', '');
-    a.setAttribute('href', blobUrl);
-    a.setAttribute('download', fileName);
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    clickDownloadElement(downloadElement(blobUrl, fileName));
 
     window.URL.revokeObjectURL(blobUrl);
     toast('File downloaded', 'green');
