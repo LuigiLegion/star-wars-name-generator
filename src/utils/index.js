f5861dd98c2416fd0501dd9d7805d38d733c4710
Tal Luigi <luigi.t.agr@gmail.com>
2022-01-15 14:36:06 -0500

    chore: renamed jsonToCsv as jsonArrayToCsvString in the utils index file

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,6 @@
 export { default as formattedFavorites } from './formatters/favorites';
 export { isValidName, randomNameByRandomRating, fullNameScore, randomInitial, randomElement } from './matchers/names';
 export { default as toast } from './materialize/toast';
-export { default as jsonToCsv } from './parsers/jsonToCsv';
+export { default as jsonArrayToCsvString } from './parsers/jsonArrayToCsvString';
 export { default as copyToClipboard } from './window/copyToClipboard';
 export { default as downloadDataFile } from './window/downloadDataFile';
