77e8edaf1bd9328644f67b828ed59a9bbf38cab3
Tal Luigi <luigi.t.agr@gmail.com>
2022-01-17 18:39:42 -0500

    feat: refactored the Navbar component to use the Links and LinksBurger components

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,16 +4,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import {
-  SignedInLinks,
-  SignedInLinksBurger,
-  SignedOutLinks,
-  SignedOutLinksBurger,
+  Links,
+  LinksBurger,
   Logo,
   Preloader,
 } from '..';
 
 // Component
-const Navbar = ({ uid, isLoading }) => {
+const Navbar = ({ isLoading }) => {
   const [width, setWidth] = useState(window.innerWidth);
 
   const isLargeView = width > 1024;
@@ -32,25 +30,13 @@ const Navbar = ({ uid, isLoading }) => {
     };
   }, [width]);
 
-  let links;
-
-  if (uid && isLargeView) {
-    links = <SignedInLinks />;
-  } else if (uid) {
-    links = <SignedInLinksBurger />;
-  } else if (isLargeView) {
-    links = <SignedOutLinks />;
-  } else {
-    links = <SignedOutLinksBurger />;
-  }
-
   return (
     <div className="navbar-fixed">
       <nav className="nav-wrapper black">
         <div>
           <Logo isLargeView={isLargeView} />
 
-          {links}
+          {isLargeView ? <Links /> : <LinksBurger />}
         </div>
 
         <div>{isLoading && <Preloader />}</div>
@@ -61,13 +47,11 @@ const Navbar = ({ uid, isLoading }) => {
 
 // Container
 const mapStateToProps = state => ({
-  uid: state.firebase.auth.uid,
   isLoading: state.layout.isLoading,
 });
 
 // Prop Types
 Navbar.propTypes = {
-  uid: PropTypes.string,
   isLoading: PropTypes.bool,
 };
 
